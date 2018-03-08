65c61127afffec7c76505e4ab5418d967f55c708
Marcelo Brandalero <b.marcelo@gmail.com>
2018-03-08 13:34:57 -0300

    Added gem5 c-style to setup-development.el.

--- custom/setup-development.el
+++ custom/setup-development.el
@@ -1,1 +1,18 @@
 (provide 'setup-development)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; GROUP: Development -> Projects -> gem5 ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(c-add-style "m5"
+	     '((c-basic-offset . 4)
+               (indent-tabs-mode . nil)
+	       (c-offsets-alist . ((substatement-open . 0)
+				   (inline-open . 0)
+				   (block-open . -4)
+				   (case-label . 2)
+				   (label . 2)
+				   (statement-case-intro . 2)
+				   (statement-case-open . 2)
+				   (access-label . -2)
+				   (innamespace . 0)))))
